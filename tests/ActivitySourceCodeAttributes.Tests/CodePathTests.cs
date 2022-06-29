f564a68f6b5284c41d275abfd18ca6841b86b536
Martin Thwaites <github@my2cents.co.uk>
2022-06-29 21:39:35 +0100

    Added Test for codeUrl attribute

--- tests/ActivitySourceCodeAttributes.Tests/CodePathTests.cs
+++ tests/ActivitySourceCodeAttributes.Tests/CodePathTests.cs
@@ -48,4 +48,24 @@ public class CodePathTests : ActivitySourceBaseTest
         testActivity.ShouldNotBeNull();
         testActivity.ShouldContainTagWithValue("code.function", nameof(StartActivity_ProducesMethodAttributeWithRightValue));
     }
+
+    [Fact]
+    public void StartActivity_ProducesCodeUrlAttributeWithRightValue()
+    {
+        AddListenerToTestSource();
+
+        var span = _source.StartActivity("TestActivity");
+        span.ShouldNotBeNull();
+        span?.Stop();
+
+        var testActivity = triggeredActivities.FirstOrDefault();
+        testActivity.ShouldNotBeNull();
+        testActivity.Tags.ShouldContain(t => t.Key == "code.url");
+        var codeurl = testActivity.Tags.First(t => t.Key == "code.url").Value;
+
+        var thisFile = new FileInfo(GetFilePath());
+        codeurl.ShouldStartWith("https://github.com/martinjt");
+        var codeUrlWithoutLine = codeurl.Split('#')[0];
+        codeUrlWithoutLine.ShouldEndWith(thisFile.Name);
+    }
 }
